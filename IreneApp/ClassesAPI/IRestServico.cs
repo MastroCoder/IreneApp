9abcef9647d5c3393a1f71ee419f04da9619f094
MastroCoder <54457201+MastroCoder@users.noreply.github.com>
2020-06-16 22:32:01 -0300

    Em essência o galho antigo
    Mudanças leves de preparação. Provavelmente não builda.

---
+++ IreneApp/ClassesAPI/IRestServico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IreneApp.ClassesAPI
+{
+    public interface IRestServico
+    {
+        /*Task<List<NomeClasse>> RefreshDataAsync();
+        Task SaveTodoItemAsync(NomeCLasse item, bool isNewItem);
+
+        Task DeleteTodoItemAsync(string id);
+        */
+    }
+}
---
+++ IreneApp/ClassesAPI/ToDoModelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IreneApp.ClassesAPI
+{
+    public class ToDoModelo
+    {
+        int ID_Classe { get; set; }
+    }
+}
