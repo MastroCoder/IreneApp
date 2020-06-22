dff24772823af1b18c5f233eb22568e74419dd64
MastroCoder <54457201+MastroCoder@users.noreply.github.com>
2020-06-22 16:41:54 -0300

    22/6

--- IreneApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ IreneApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "ed67ab0e-cfe0-4157-92d7-e2e474297538",
+		"XAMARIN_BUILD_ID", "16a2690f-1cf1-4688-bfff-12ee53bbba0e",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
