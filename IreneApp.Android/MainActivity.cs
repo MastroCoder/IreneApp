2df3502151a01633460f26df838e22f81f959dd0
MastroCoder <54457201+MastroCoder@users.noreply.github.com>
2020-06-30 20:02:52 -0300

    30/6

--- IreneApp.Android/MainActivity.cs
+++ IreneApp.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms;
 
 namespace IreneApp.Droid
 {
-    [Activity(Label = "IreneApp", Icon = "@mipmap/teste", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "IreneApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- IreneApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ IreneApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "16a2690f-1cf1-4688-bfff-12ee53bbba0e",
+		"XAMARIN_BUILD_ID", "ba4e1d67-44bc-49a7-8c0a-d0ab75a751d5",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
